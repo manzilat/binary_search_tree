4a893d4d16d099a62424aef7032f32b6b9ef5d6a
manzilat <amtulnoorq@yahoo.com>
2018-10-12 09:23:54 -0500

    initial and final commit

---
+++ BST/BST/BinaryTree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class BinaryTree<T> where T : IComparable
+    {
+        Node<T> root;
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public Node<T> Root
+        {
+            get
+            {
+                return root;
+            }
+            set
+            {
+                root = value;
+            }
+
+        }
+        public virtual void Clear()
+        {
+            root = null;
+        }
+
+        public void Insert(T value)
+        {
+            var Node = new Node<T>(value);
+            Insert(Node);
+        }
+        public void Insert(Node<T> node)
+        {
+            if (Root == null)
+            {
+                Root = node;
+                return;
+            }
+
+            // Insert(node, Root);
+        }
+
+    }
+}
---
+++ BST/BST/Node.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+
+    public class Node<T> where T : IComparable
+    {
+        T data;
+        Node<T> left = null;
+        Node<T> right = null;
+
+        public Node() { }
+        public Node(T data) : this(data, null, null) { }
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+
+        public Node<T> Right
+        {
+            get
+            {
+                return right;
+            }
+            set
+            {
+                right = value;
+            }
+        }
+
+        public Node<T> Left
+        {
+            get
+            {
+                return left;
+            }
+            set
+            {
+                left = value;
+            }
+        }
+
+        public void Insert(Node<T> node)
+        {
+            if (Value == null)
+            {
+                Value = node.data;
+                Right = node.Right;
+                Left = node.Left;
+
+                return;
+            }
+
+            if (node.data.CompareTo(Value) <= 0)
+            {
+                Left.Insert(node);
+                return;
+            }
+
+            Right.Insert(node);
+        }
+
+
+    }
+}
